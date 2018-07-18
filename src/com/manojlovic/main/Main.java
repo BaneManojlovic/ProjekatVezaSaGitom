8678ffa8b5d6bcc7354c1a8ac6256ed75d8c95a6
Branislav Manojlovic <bane1manojlovic@gmail.com>
2018-07-18 23:32:12 +0200

    Second commit

--- src/com/manojlovic/main/Main.java
+++ src/com/manojlovic/main/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello World oticicu na GIT");
+        System.out.println("Hello idem i ja na Git u drugom commit-u");
     }
 }
