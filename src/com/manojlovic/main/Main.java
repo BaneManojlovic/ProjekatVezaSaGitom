fcc50e933b67ed1d999ad9ae1d8dd310298bf77e
Branislav Manojlovic <bane1manojlovic@gmail.com>
2018-07-18 23:22:26 +0200

    Initial commit

---
+++ src/com/manojlovic/main/Main.java
@@ -0,0 +1,9 @@
+package com.manojlovic.main;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello World oticicu na GIT");
+    }
+}
